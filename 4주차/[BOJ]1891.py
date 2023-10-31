e7166ccb270c49d79bd9fbcfb2c53ed020e261f6
cho1n <cjaewon0217@naver.com>
2023-10-31 17:22:03 +0900

    [BOJ] / 샤워실 바닥 깔기(small) / Silver1 / <https://www.acmicpc.net/problem/14600>

--- 4주차/[BOJ]1891.py
+++ 4주차/[BOJ]1891.py
@@ -1,47 +1,75 @@
-def find(n1, n2, m1, m2, idx):
-   if idx == len(number):
-       return n1, m1
+import sys
 
-   if number[idx] == '1':
-       return find(n1, (n1+n2)//2, (m1+m2)//2, m2, idx+1)
-   elif number[idx] == '2':
-       return find(n1, (n1+n2)//2, m1, (m1+m2)//2, idx+1)
-   elif number[idx] == '3':
-       return find((n1+n2)//2, n2, m1, (m1+m2)//2, idx+1)
-   elif number[idx] == '4':
-       return find((n1+n2)//2, n2, (m1+m2)//2, m2, idx+1)
 
-def check(n1, n2, m1, m2):
+def read_line():
+    return sys.stdin.readline().strip()
+
+
+def coordinate(idx, sx, ex, sy, ey):
+    """
+    param idx:
+    param sx, ex: 사분면의 범위가 sx ~ ex
+    param sy, ey: 사분면의 범위가 sy ~ ey
+    return: 기저사례에서 최종적인 사분면의 좌표 반환
+    """
+
+    if d == idx:
+        return sx, sy
+
+    half_x = (sx + ex) // 2
+    half_y = (sy + ey) // 2
+
+    if arr[idx] == "1":
+        return coordinate(idx + 1, half_x, ex, sy, half_y)
+    elif arr[idx] == "2":
+        return coordinate(idx + 1, sx, half_x, sy, half_y)
+    elif arr[idx] == "3":
+        return coordinate(idx + 1, sx, half_x, half_y, ey)
+    elif arr[idx] == "4":
+        return coordinate(idx + 1, half_x, ex, half_y, ey)
+
+
+def quadrant(sx, ex, sy, ey):
+    """
+    param sx, ex: 사분면의 범위가 sx ~ ex
+    param sy, ey: 사분면의 범위가 sy ~ ey
+    return: 기저사례에서 최종적인 사분면 번호 반환
+    """
+
     global answer
-    if len(answer) == int(d):
+    if len(answer) == d:
         return answer
 
-    if n1 <= nx < (n1+n2)//2 and (m1+m2)//2 <= ny < m2:
-        answer += '1'
-        return check(n1, (n1 + n2) // 2, (m1 + m2) // 2, m2)
+    half_x = (sx + ex) // 2
+    half_y = (sy + ey) // 2
 
-    elif n1 <= nx < (n1+n2)//2 and m1 <= ny < (m1+m2)//2:
-        answer += '2'
-        return check(n1, (n1 + n2) // 2, m1, (m1 + m2) // 2)
+    if half_x <= nx < ex and sy <= ny < half_y:
+        answer += "1"
+        return quadrant(half_x, ex, sy, half_y)
+    elif sx <= nx < half_x and sy <= ny < half_y:
+        answer += "2"
+        return quadrant(sx, half_x, sy, half_y)
+    elif sx <= nx < half_x and half_y <= ny < ey:
+        answer += "3"
+        return quadrant(sx, half_x, half_y, ey)
+    elif half_x <= nx < ex and half_y <= ny < ey:
+        answer += "4"
+        return quadrant(half_x, ex, half_y, ey)
 
-    elif (n1+n2)//2 <= nx < n2 and m1 <= ny < (m1+m2)//2:
-        answer += '3'
-        return check((n1 + n2) // 2, n2, m1, (m1 + m2) // 2)
 
-    elif (n1+n2)//2 <= nx < n2 and (m1+m2)//2 <= ny < m2:
-        answer += '4'
-        return check((n1 + n2) // 2, n2, (m1 + m2) // 2, m2)
+d, q_num = read_line().split()
+d = int(d)
+x, y = map(int, read_line().split())
+arr = [i for i in q_num]
 
-d, number = input().split()
-x, y = map(int, input().split())
+n = 1 << d  # 2^d
 
-n, m = 2**int(d), 2**int(d)
+dx, dy = coordinate(0, 0, n, 0, n)
+nx, ny = dx + x, dy - y
 
-dx, dy = find(0, n, 0, m, 0) # 사분면 조각의 좌표
-nx, ny = (-1*y) + dx, x + dy # 새로운 사분면 조각의 좌표
-answer = ''
+answer = ""
 
-if 0 <= nx < n and 0 <= ny < m:
-    print(int(check(0, n, 0, m)))
+if 0 <= nx < n and 0 <= ny < n:
+    print(int(quadrant(0, n, 0, n)))
 else:
     print(-1)
