05a342a29e2692842c645b092f81af17033f3a9e
Github <alswkdrb@dgu.ac.kr>
2023-11-05 16:35:57 +0900

    [PRGMS] / 입국심사 / level3 / https://school.programmers.co.kr/learn/courses/30/lessons/43238

--- 민장규/4주차/[PRGMS] 43238.py
+++ 민장규/4주차/[PRGMS] 43238.py
@@ -2,6 +2,33 @@ import heapq as hq
 
 def solution(n, times):
     heap = []
+    
+    avg = 0
+    for t in times :
+        avg += 1.0 / t  
+    Sum = (1/avg) * n
+    
+    for i in times :
+        cnt = int(Sum) // i
+        if cnt <= 0 :
+            continue
+        hq.heappush(heap, [i*(cnt+1), cnt])
+        n -= cnt
+    
+    answer = heap[0][0] - (heap[0][0] // (heap[0][1] + 1))
+    for _ in range(n) :
+        cnt, num = hq.heappop(heap)
+        answer = cnt
+        hq.heappush(heap, [cnt+(cnt//(num+1)), num+1])
+    return answer
+
+print(solution(22, [51, 510]))
+
+'''
+import heapq as hq
+
+def solution(n, times):
+    heap = []
     for i in times :
         hq.heappush(heap, [i, 1])
     
@@ -11,3 +38,4 @@ def solution(n, times):
         answer = cnt
         hq.heappush(heap, [cnt+cnt//num, num+1])
     return answer
+'''
