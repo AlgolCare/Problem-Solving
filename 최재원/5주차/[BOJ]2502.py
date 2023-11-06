a56d6e7f64e4393a59cb9fb0b730ba906dae704c
cho1n <cjaewon0217@naver.com>
2023-11-06 21:29:44 +0900

    [BOJ] / 떡 먹는 호랑이 / Silver1 / <https://www.acmicpc.net/problem/2502\>

---
+++ 최재원/5주차/[BOJ]2502.py
@@ -0,0 +1,15 @@
+D, K = map(int, input().split())
+
+for i in range(1, 100000):
+    for j in range(1, 100000):
+        dp = [0] * (D + 1)
+        dp[0] = i
+        dp[1] = j
+
+        for l in range(2, D + 1):
+            dp[l] = dp[l - 1] + dp[l - 2]
+
+        if dp[-1] == K:
+            print(dp[1])
+            print(dp[2])
+            exit()
