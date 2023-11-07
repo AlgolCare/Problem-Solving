fd6d658bd0506b6cd0c0e7a84f500925cc2ef638
dlawjddn <jwl5015@naver.com>
2023-11-07 17:27:00 +0900

    [BOJ] / 패셔니스타 / gold4 / https://www.acmicpc.net/problem/5535

--- 임정우/5주차/[BOJ] 5535.cpp
+++ 임정우/5주차/[BOJ] 5535.cpp
@@ -1,37 +1,43 @@
 #include <iostream>
-#include <algorithm>
 #include <cstring>
-#include <cmath>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int days=0, clothes=0, result=0;
-int temperture[201], attribute[201], dp[201][201];
+int n, d;
+int temp[201], min_temp[201], max_temp[201], att[201], dp[101][201];
 
-int main(){
-    cin>>days>>clothes;
-    for(int i = 0; i <= days; i++) {
-        for(int j = 0; j <= clothes; j++) {
-            dp[i][j] = -1;
-        }
+// 재귀를 사용한 DP
+int solve(int a, int day) {
+    // d 날짜가 됐다면 return
+    if (day == d)
+        return 0;
+    // 메모이제이션 활용
+    if (dp[a][day] != -1)
+        return dp[a][day];
+    int ret = 0;
+    // 전체 옷들을 순회한 후 가장 큰 값을 return 값으로 사용
+    for (int i = 0; i < n; i++) {
+        if (temp[day] < min_temp[i] || temp[day] > max_temp[i])
+            continue;
+        int temp_value = solve(att[i], day + 1) + abs(a - att[i]);
+        ret = max(ret, temp_value);
     }
-    for(int i=1; i<=days; i++){
-        cin>>temperture[i];
-    }
-    for(int i=1; i<=clothes; i++){
-        int from=0, to=0, att=0;
-        cin>>from>>to>>att;
-        for(int j=1; j<=days; j++){
-            if (from <= temperture[j] && to >= temperture[j]){
-                dp[j][i] = 0;
-            }
-        }
-    }
+    return dp[a][day] = ret;
+}
 
-    for(int y=2; y<=days; y++){
-        for(int x=1; x<=clothes; x++){
-            if (dp[y][x] == -1) continue;
-            
-        }
+int main() {
+    cin >> d >> n;
+    for (int i = 0; i < d; i++) cin >> temp[i];
+    for (int i = 0; i < n; i++) cin >> min_temp[i] >> max_temp[i] >> att[i];
+    memset(dp, -1, sizeof(dp));
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (temp[0] < min_temp[i] || temp[0] > max_temp[i])
+            continue;
+        int temp_value = solve(att[i], 1);
+        ans = max(ans, temp_value);
     }
-
+    cout << ans;
+    return 0;
 }
